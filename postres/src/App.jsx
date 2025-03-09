272d120e5d1cce876d4113f5c4ed8b74b838c320
FoxyYTs <foxy200442@gmail.com>
2025-03-09 05:49:21 -0500

    a

--- postres/src/App.jsx
+++ postres/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   });
 
   // Función para obtener datos del backend
-  const fetchData = async () => {
+  const fetchData = async () => {s
     setLoading(true);
     try {
       const response = await fetch('http://localhost:5000/api/data');
